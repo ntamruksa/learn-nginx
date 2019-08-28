de1421362c6852c8d34eb967bf0fc0f47a9b8d29
ntamruksa <ptamruksa@gmail.com>
2019-08-28 10:29:49 +1000

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,17 +27,6 @@ function App() {
             return <li key={user.id}>{user.name}</li>
           })}
         </ul>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
